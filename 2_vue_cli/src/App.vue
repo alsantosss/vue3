7bfdb6cc0a9096cdbc90e450740d79e8bd81df6f
Andre Luis dos Santos <alsantosss@hotmail.com>
2021-08-04 16:45:27 -0300

    criando primeiro componente

--- 2_vue_cli/src/App.vue
+++ 2_vue_cli/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <primeiroComponente/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import primeiroComponente from './components/primeiroComponente.vue'
+  export default{
+    name: 'App',
+    components: {
+      primeiroComponente
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

325e5146a4d10d621a050609cea483908e3ecc36
Andre Luis dos Santos <alsantosss@hotmail.com>
2021-08-07 12:19:49 -0300

    estruturas repetiçao / v-bind / imagens

---
+++ 2_vue_cli/src/components/avatar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img :src="avatar" :title="avatar_desc" width="100" height="120">
+    </div>
+</template>
+
+<script>
+export default {
+    nome:"avatar",
+    data(){
+        return {
+            avatar: "/img/avatar.svg",
+            avatar_desc:"avatar"
+        }
+    }
+}
+</script>
--- 2_vue_cli/src/components/info.vue
+++ 2_vue_cli/src/components/info.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades.</p>
         <p>Utilizo as seguintes Tecnologias</p>
         <ul>
             <li>PHP</li>
@@ -9,11 +10,24 @@
             <li>HTML</li>
             <li>CSS</li>
         </ul>
+        <p v-if="2>1">Dois é maior que um</p>
+        <p v-show="email">Email: alsantosss@gmail.com</p>
+        <p>Portfólio: <a v-bind:href="meu_link" target="_blank">Clique para ver o portfólio</a> </p>
+        <avatar />
     </div>
 </template>
 
 <script>
+import avatar from './avatar.vue'
 export default {
-    nome: "info"    
+  components: { avatar },
+    nome: "info",
+    data(){
+        return {
+            trabalhando: false,
+            email:true,
+            meu_link: "https://google.com.br"
+        }
+    } 
 }
 </script>
